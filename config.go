74722601194e6c0689058eb8e8c0c2a6fc9c1e02
Minori Tokuda <minori.tokuda@origami.com>
2018-04-26 14:18:25 +0900

    print config unless quiet flag passed

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/naoina/toml"
 	"io/ioutil"
 	"net"
@@ -25,7 +26,7 @@ type Tunnel struct {
 	callback      func(net.Addr) `toml:"-"`
 }
 
-func LoadConfig(confpath string) (map[string]BastionConfig, error) {
+func LoadConfig(confpath string, printconf bool) (map[string]BastionConfig, error) {
 	config := make(map[string]BastionConfig)
 	data, err := ioutil.ReadFile(confpath)
 	if err != nil {
@@ -34,6 +35,10 @@ func LoadConfig(confpath string) (map[string]BastionConfig, error) {
 	err = toml.Unmarshal(data, &config)
 	if err != nil {
 		return config, err
+	}
+
+	if printconf {
+		fmt.Println(string(data))
 	}
 	return config, nil
 }
--- main.go
+++ main.go
@@ -9,9 +9,11 @@ func main() {
 	var confpath string
 	flag.StringVar(&confpath, "c", "./tunneler.toml", "Path to config file(shorthand)")
 	flag.StringVar(&confpath, "config", "./tunneler.toml", "Path to config file")
+	var quiet bool
+	flag.BoolVar(&quiet, "q", false, "Don't show the config content")
 	flag.Parse()
 
-	config, err := LoadConfig(confpath)
+	config, err := LoadConfig(confpath, !quiet)
 	if err != nil {
 		log.Println("Failed to load a config file:", confpath, err)
 		return
